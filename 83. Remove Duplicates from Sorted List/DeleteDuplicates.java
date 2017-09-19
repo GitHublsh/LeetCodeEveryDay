bae63706b2d008acc64476f3b3ecaeefbf7902f6
lsh <liu601545126@qq.com>
2017-09-19 15:06:35 +0800

    【LeetCode 】 83. Remove Duplicates from Sorted List

--- 83. Remove Duplicates from Sorted List/DeleteDuplicates.java
+++ 83. Remove Duplicates from Sorted List/DeleteDuplicates.java
@@ -1,3 +1,4 @@
+ //移除已排好序的LinkedList中重复的元素
  public class ListNode {
         int val;
         ListNode next;
@@ -7,9 +8,9 @@
         }
     }
 
-    //移除已排好序的LinkedList中重复的元素
-    public static ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) return head;
-        head.next = deleteDuplicates(head.next);
-        return head.val == head.next.val ? head.next : head;
-    }
+    
+public static ListNode deleteDuplicates(ListNode head) {
+    if (head == null || head.next == null) return head;
+    head.next = deleteDuplicates(head.next);
+    return head.val == head.next.val ? head.next : head;
+}
