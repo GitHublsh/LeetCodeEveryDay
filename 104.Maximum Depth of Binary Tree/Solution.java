a894c6f282ce2171040364d92af8445e0b2204ac
CodeNeil <codeneil@163.com>
2018-06-04 15:45:36 +0800

    Update Solution.java

--- 104.Maximum Depth of Binary Tree/Solution.java
+++ 104.Maximum Depth of Binary Tree/Solution.java
@@ -1,7 +1,18 @@
- //二叉树的最大深度
+ //二叉树的最大深度 解法一
     public static int maxDepth(TreeNode root) {
             if(root==null){
                 return 0;
             }
             return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+
+//解法二
+public int maxDepth(TreeNode root) {
+        if (null == root) {
+            return 0;
+        } else {
+            int i = maxDepth(root.left);
+            int j = maxDepth(root.right);
+            return (i < j) ? j + 1 : i + 1;
+        }
     }
